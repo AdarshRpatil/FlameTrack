ff2b4d5730f824a8c323344dbeb0a64a516efc96
Adarsh Patil <adarshrpatil02@gmail.com>
2024-10-10 10:23:48 +0530

    Added date and time in location box

--- src/components/LocationInfoBox.js
+++ src/components/LocationInfoBox.js
@@ -3,11 +3,13 @@ const LocationInfoBox = ({ info }) => {
         <div className="location-info">
             <h3>Event Location Info</h3>
             <ul>
-    <li>ID: <strong>{ info.id }</strong></li>
-    <li>TITLE: <strong>{ info.title }</strong></li>
+                <li>ID: <strong>{info.id}</strong></li>
+                <li>TITLE: <strong>{info.title}</strong></li>
+                <li>DATE: <strong>{info.date}</strong></li>
             </ul>
         </div>
     )
 }
+
 
 export default LocationInfoBox
--- src/components/Map.js
+++ src/components/Map.js
@@ -11,7 +11,7 @@ const Map = ({ eventData, center, zoom }) => {
             key={ev.id}
             lat={ev.geometries[0].coordinates[1]}
              lng={ev.geometries[0].coordinates[0]}
-            onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
+            onClick={() => setLocationInfo({ id: ev.id, title: ev.title, date: new Date(ev.geometries[0].date).toLocaleString() })} />
         }
         return null
     })
